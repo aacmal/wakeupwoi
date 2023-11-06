7bc5d39c59e3d746a78b19b671cd0509d17b38d7
Aca M <acamaulana123@gmail.com>
2023-11-06 21:38:49 +0700

    added notification when alarm triggered

--- app/src/main/java/com/acml/wakeupwoi/service/AlarmReceiver.kt
+++ app/src/main/java/com/acml/wakeupwoi/service/AlarmReceiver.kt
@@ -1,10 +1,12 @@
 package com.acml.wakeupwoi.service
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.media.MediaPlayer
 import android.media.RingtoneManager
+import androidx.core.app.NotificationCompat
 
 
 class AlarmReceiver: BroadcastReceiver() {
@@ -13,7 +15,24 @@ class AlarmReceiver: BroadcastReceiver() {
         println("AlarmReceiver: $message")
 
         val notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM)
-        val mp = MediaPlayer.create(context, notification)
-        mp.start()
+
+        // Create a notification channel
+        val channelId = "default"
+        val channelName = "Default Channel"
+        val importance = NotificationManager.IMPORTANCE_HIGH
+        val channel = NotificationChannel(channelId, channelName, importance)
+        val notificationManager = context?.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
+
+        // Create a notification builder
+        val builder = NotificationCompat.Builder(context, channelId)
+            .setSmallIcon(android.R.drawable.ic_dialog_info)
+            .setContentTitle("Alarm Notification")
+            .setContentText("Alarm is ringing!")
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setSound(notification)
+
+        // Show the notification
+        notificationManager.notify(0, builder.build())
     }
 }
